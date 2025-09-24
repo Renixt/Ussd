7489cbb0306d621d6a74c201d9a88f401bd43e53
renata <renata_castillo_@outlook.com>
2025-09-23 21:47:22 -0600

    edicion de menu 2

--- index.php
+++ index.php
@@ -41,8 +41,8 @@ if ($text === '') {
     $accountNumber = explode('*', $text)[2]; // Toma lo que está después de 1*1
     $response = "CON Tu número de cuenta capturado es: " . $accountNumber;
     $text = substr($text, 0, 4); //regresa text a 1*1*
-    $response = "CON Ingresa '1' para continuar";
-} elseif ($text === '1*1') {
+    $response = "CON Ingresa '1' para continuar" .$text;
+} elseif ($text === '1*1*1') {
         $response = "END Codigo actual:" .$text;
 
 }
